936b8f9e3d4fc89f46edacec76adc7c5a3000151
noicerdoicer <101971231+noicerdoicer@users.noreply.github.com>
2022-04-26 10:47:59 +0200

    cc3

--- 01-Fundamentals-Part-1/starter/cc3.js
+++ 01-Fundamentals-Part-1/starter/cc3.js
@@ -6,13 +6,16 @@ Test data:
 
 Your tasks:
 1. Calculate the average score for each team, using the test data below
+
 2. Compare the team's average scores to determine the winner of the competition,
 and print it to the console. Don't forget that there can be a draw, so test for that
 as well (draw means they have the same average score)
+
 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
 team only wins if it has a higher score than the other team, and the same time a
 score of at least 100 points. Hint: Use a logical operator to test for minimum
 score, as well as multiple else-if blocks 😉
+
 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
 both teams have the same score and both have a score greater or equal 100
 points. Otherwise, no team wins the trophy
@@ -44,7 +47,8 @@ const Koalas3 = 101;*/
 /*const Koalas1 = 109;
 const Koalas2 = 95;
 const Koalas3 = 123;*/
-//Set 3 
+
+/*//Set 3 
 const Koalas1 = 109;
 const Koalas2 = 95;
 const Koalas3 = 106;
@@ -60,14 +64,14 @@ console.log(avgDolphins, avgKoalas);
 
 
 //2. Avg vergleichen
-if (avgDolphins > avgKoalas && avgDolphins > 100) {
+if (avgDolphins > avgKoalas && avgDolphins >= 100) {
     console.log('Dolphin avg ist größer.');
 }
-else if (avgDolphins == avgKoalas && (avgDolphins && avgDolphins > 100)) {
+else if (avgDolphins == avgKoalas && (avgDolphins && avgDolphins >= 100)) {
     console.log('Beide avg sind gleich.');
 }
 
-else if (avgKoalas > avgDolphins && avgKoalas > 100) {
+else if (avgKoalas >= avgDolphins && avgKoalas >= 100) {
     console.log('Koala avg ist größer.');
 }
 else {
