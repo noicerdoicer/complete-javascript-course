5d3eb439a92b7f0bb7f3f214df8b9906e59498de
noicerdoicer <101971231+noicerdoicer@users.noreply.github.com>
2022-05-03 16:28:18 +0200

    Lernen Fundamentals 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
+if(passTest) hasDriverLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+*/
+
+/*
+//Funktion deklarieren 
+function logger(){
+console.log('My name is Jonas');
+}
+
+//Funktion ausführen , aufrufen etc...
+logger();
+
+
+function fruiteProcessor(apples,oranges){
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges `;
+    return juice;
+}
+const appleJuice = fruiteProcessor(5,20);
+console.log(appleJuice)
+
+//Funktion helfen bei der Einhaltung des DRY Prinzip --> Dont repeat yourself
+*/
+
+
+function calcAge1 (birthYear){
+    return 2037 - birthYear;
+    // Man kann Werte direkt ausrechen und über return zurückgeben
+}
+
+calcAge1(1998);
