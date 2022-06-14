ab842e1564405c1c18adebc8c24705cf092cc039
noicerdoicer <101971231+noicerdoicer@users.noreply.github.com>
2022-06-14 20:00:09 +0200

    letzte Übung 04 / Beginn 05

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//Damit holt man sich den Textinhalt des .message HTML-Objekt
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number !';
+
+//Werte auf der Webseite per querySelector manipulieren
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.number').textContent);
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
