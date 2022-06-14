ecb441a1aa65e47c54f2785ca1828a3e420bbc7a
noicerdoicer <101971231+noicerdoicer@users.noreply.github.com>
2022-06-14 20:15:43 +0200

    first dom manipulation example leading to guessing numbers

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,3 +13,17 @@ console.log(document.querySelector('.number').textContent);
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+//Per Eventlistener wird bei Click des Buttons der Value des Textfeldes in der Kosnole geloggt
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number !';
+  }
+});
