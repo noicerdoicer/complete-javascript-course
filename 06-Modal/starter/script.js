0e666658b49cb7c022f035fcdb124bf98b9899da
noicerdoicer <101971231+noicerdoicer@users.noreply.github.com>
2022-08-04 18:02:53 +0200

    Modal Window Projekt

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+  //X Button in Modal Fenster schließt Fenster
+  //Nicht Funktion sondern Variable aufrufen ,
+  // da sonst direkt Fenster durch Funktionsaufruf geschlossen wird
+  btnCloseModal.addEventListener('click', closeModal);
+
+  //Overlay clicken um Fenster zu schließen
+  overlay.addEventListener('click', closeModal);
+}
+
+//mit dem Eventlistener 'keydown' prüft man ob eine Taste gedrückt wird.
+//in der anonymen Funktion wird der Tastendruck als Parameter der Funktion übergeben
+//Bei einem Event erstellt Jaava Script ein Objekt für das Keydown Event
+document.addEventListener('keydown', function (e) {
+  //e.key ruft im Keydown Event Objekt die Eigenschaft key aus , die die gedrückte Taste enhält
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
